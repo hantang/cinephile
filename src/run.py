971b7f4e6fc921891f3528de1d400988363c6c35
hantang <34468720+hantang@users.noreply.github.com>
2023-10-18 22:50:34 +0800

    Update src

--- src/run.py
+++ src/run.py
@@ -1,8 +1,7 @@
 import argparse
-import datetime
 import logging
-from pathlib import Path
 import traceback
+from pathlib import Path
 
 from crawlers import (
     DoubanCrawler,
@@ -12,13 +11,14 @@ from crawlers import (
     TmdbCrawler,
     DoubanWeeklyCrawler,
 )
+from utils import get_table, get_dt
 
 BASEDIR = ".."
 SITES = ["douban", "imdb", "mtime", "maoyan", "tmdb"]
 EXTRAS = ["douban-weekly"]
 
 
-def download(savedir, sites):
+def download_stats(savedir, sites):
     stats = {}
     savedir = "{}/{}/".format(BASEDIR, savedir.split("./")[-1].strip("/"))
     for site in sites:
@@ -49,51 +49,16 @@ def download(savedir, sites):
     return stats
 
 
-def create_md_table(table, index=True):
-    sep = " | "
-    min_len = 2
-    row_cnt = max([len(col) for col in table])
-    header = ["**{}**".format(col[0]) for col in table]
-    if index:
-        header = ["**index**"] + header
-    lines = [
-        sep.join(header),
-        sep.join([":{}:".format("-" * min(len(h), min_len)) for h in header]),
-    ]
-    for i in range(1, row_cnt):
-        row = []
-        for col in table:
-            if i >= len(col):
-                text = " " * min_len
-            else:
-                cell = col[i]
-                if isinstance(cell, list):
-                    img = f"![img]({cell[0]})" + "<br/>"
-                    alt = cell[1]
-                else:
-                    img = ""
-                    alt = cell
-                text = img + alt + " " * max(0, min_len - len(alt))
-            row.append(text)
-        if index:
-            row = [f"{i:3d}"] + row
-        lines.append(sep.join(row))
-    lines = [f"|{line}|" for line in lines]
-
-    return "\n".join(lines)
-
-
-def readme(dt, stats):
+def update_readme(stats):
     readfile = Path(f"{BASEDIR}/README.md")
     hr_line = "-" * 3
     limit = 2
-    temp = []
+    raw_readmes = []
     if readfile.exists():
         with open(readfile) as f:
-            temp = f.readlines()[:limit]
-    temp += ["\n" * max(0, limit - len(temp))]
-    first = "".join(temp)
-    logging.info(first)
+            raw_readmes = f.readlines()[:limit]
+    raw_readmes += ["\n" * max(0, limit - len(raw_readmes))]
+    logging.info(f"readme = {raw_readmes}")
 
     logging.info("create table, part1")
     part1 = []
@@ -109,7 +74,7 @@ def readme(dt, stats):
         for entry in items:
             subdesc, subitems = entry["desc"], entry["items"]
             table.append([subdesc] + subitems)
-        md_table = create_md_table(table)
+        md_table = get_table(table)
         part1 += [f"## {desc}", md_table]
 
     logging.info("create table, part2")
@@ -121,19 +86,14 @@ def readme(dt, stats):
         desc, items = data["desc"], data["items"]
         table2.append([desc] + items)
         logging.info(f"table {site}, desc={desc}")
-    md_table2 = create_md_table(table2)
+    md_table2 = get_table(table2)
     part2 = ["## 电影Top榜单", md_table2]
 
-    md_out = (
-        [
-            first,
-            "Last update: {}".format(dt.strftime("%Y-%m-%dT%H:%M:%SZ")),
-            hr_line,
-        ]
-        + part1
-        + [hr_line]
-        + part2
-    )
+    md_out = ["".join(raw_readmes).strip(), "最近更新：{}".format(get_dt())]
+    if part1:
+        md_out += [hr_line] + part1
+    if part2:
+        md_out += [hr_line] + part2
 
     logging.info(f"save to {readfile}")
     with open(readfile, "w") as fw:
@@ -154,8 +114,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     logging.info(f"args = {args}\n")
 
-    dt = datetime.datetime.utcnow()
-    stats = download(args.savedir, args.sites)
+    stats = download_stats(args.savedir, args.sites)
     if len(stats) > 0:
-        readme(dt, stats)
+        update_readme(stats)
     logging.info(f"done\n")
