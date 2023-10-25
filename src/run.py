79224bf49b0b49475160c67c8ce53437519debee
ht <ht@starrytribe.com>
2023-10-25 19:39:50 +0800

    Fix tmbd*

--- src/run.py
+++ src/run.py
@@ -1,6 +1,8 @@
 import argparse
 import json
 import logging
+import time
+import traceback
 from pathlib import Path
 
 from cinephile.crawlers import (
@@ -25,27 +27,35 @@ def download_stats(savedir, sites, retry=2):
     for site in sites:
         logging.info("process:\n" + "\n".join(["=" * 50, " " * 20 + site, "=" * 50]))
         crawler, result = None, ""
-        if site == "douban":
-            crawler = DoubanCrawler()
-            result, result_file = crawler.process_top250(savedir + "douban")
-        elif site == "douban-weekly":
-            crawler = DoubanCrawler()
-            result, result_file = crawler.process_hot(savedir + "douban-weekly")
-        elif site == "imdb":
-            crawler = ImdbCrawler()
-            result, result_file = crawler.process_top250(savedir + "imdb")
-        elif site == "mtime":
-            crawler = MtimeCrawler()
-            result, result_file = crawler.process_top100(savedir + "misc")
-        elif site == "maoyan":
-            crawler = MaoyanCrawler()
-            result, result_file = crawler.process_top100(savedir + "misc")
-        elif site == "tmdb":
-            crawler = TmdbCrawler()
-            result, result_file = crawler.process_top250(savedir + "misc")
-        logging.info(f"site = {site}, result = {result}, result_file={result_file}\n")
-        if (result > 0 or result == -2) and result_file:
-            stats[site] = (result, result_file)
+        for i in range(retry):
+            try:
+                if site == "douban":
+                    crawler = DoubanCrawler()
+                    result, result_file = crawler.process_top250(savedir + "douban")
+                elif site == "douban-weekly":
+                    crawler = DoubanCrawler()
+                    result, result_file = crawler.process_hot(savedir + "douban-weekly")
+                elif site == "imdb":
+                    crawler = ImdbCrawler()
+                    result, result_file = crawler.process_top250(savedir + "imdb")
+                elif site == "mtime":
+                    crawler = MtimeCrawler()
+                    result, result_file = crawler.process_top100(savedir + "misc")
+                elif site == "maoyan":
+                    crawler = MaoyanCrawler()
+                    result, result_file = crawler.process_top100(savedir + "misc")
+                elif site == "tmdb":
+                    crawler = TmdbCrawler()
+                    result, result_file = crawler.process_top250(savedir + "misc")
+                logging.info(f"site = {site}, result = {result}, result_file={result_file}\n")
+                if (result > 0 or result == -2) and result_file:
+                    stats[site] = (result, result_file)
+                    break
+            except:
+                traceback.print_exc()
+                if i + 1 < retry:
+                    logging.info("time to sleep")
+                    time.sleep(30)
     return stats
 
 
@@ -66,6 +76,7 @@ def update_readme(stats):
     ]
     is_first = True
     for key in EXTRAS:
+        if key not in stats: continue
         file = Path(stats[key][1])
         if not file.exists():
             continue
