1d4214a6e232dc98a0185d8e2fa4ccd6a967f548
hantang <34468720+hantang@users.noreply.github.com>
2023-12-31 13:17:15 +0800

    Update run

--- src/run-crawl.py
+++ src/run-crawl.py
@@ -13,58 +13,58 @@ from cinephile.crawlers import (
 from cinephile.utils.misc import set_logging
 
 BASEDIR = ".."
-SITES = ["douban", "imdb", "mtime", "maoyan", "tmdb"]
-EXTRAS = ["douban-weekly"]
+MISCDIR = "misc"
+MAIN_SITES = ["douban", "imdb"]
+EXTRA_SITES = ["douban-weekly"]
+crawler_dict = {
+    "douban": DoubanCrawler,
+    "imdb": ImdbCrawler,
+    "mtime": MtimeCrawler,
+    "maoyan": MaoyanCrawler,
+    "tmdb": TmdbCrawler,
+    "douban-weekly": DoubanCrawler,
+}
 
 
 def download_stats(savedir, sites, retry=2):
-    stats = {}
     savedir = "{}/{}/".format(BASEDIR, savedir.split("./")[-1].strip("/"))
-    result, result_file = None, None
+    result_set = []
     for site in sites:
         logging.info("process:\n" + "\n".join(["=" * 50, " " * 20 + site, "=" * 50]))
-        crawler, result = None, ""
+        if site not in crawler_dict:
+            logging.warning(f"Ignore: {site}")
+            continue
+
+        crawler = crawler_dict[site]()
         for i in range(retry):
             try:
-                if site == "douban":
-                    crawler = DoubanCrawler()
-                    result, result_file = crawler.process_top250(savedir + "douban")
-                elif site == "douban-weekly":
-                    crawler = DoubanCrawler()
-                    result, result_file = crawler.process_hot(savedir + "douban-weekly")
-                elif site == "imdb":
-                    crawler = ImdbCrawler()
-                    result, result_file = crawler.process_top250(savedir + "imdb")
-                elif site == "mtime":
-                    crawler = MtimeCrawler()
-                    result, result_file = crawler.process_top100(savedir + "misc")
-                elif site == "maoyan":
-                    crawler = MaoyanCrawler()
-                    result, result_file = crawler.process_top100(savedir + "misc")
-                elif site == "tmdb":
-                    crawler = TmdbCrawler()
-                    result, result_file = crawler.process_top250(savedir + "misc")
-                logging.info(f"site = {site}, result = {result}, result_file={result_file}\n")
+                subsavedir = savedir + (site if site in MAIN_SITES + EXTRA_SITES else MISCDIR)
+                if site in EXTRA_SITES:
+                    result, result_file = crawler.process_hot(subsavedir)
+                else:
+                    result, result_file = crawler.process_top(subsavedir)
+
+                logging.info(f"Site = {site}, result = {result}, result_file={result_file}\n")
                 if (result > 0 or result == -2) and result_file:
-                    stats[site] = (result, result_file)
+                    result_set.append(result_file)
                     break
-            except:
+            except Exception:
                 traceback.print_exc()
                 if i + 1 < retry:
                     logging.info("time to sleep")
                     time.sleep(30)
-    return stats
+
+        files = "\n- ".join(map(str, result_set))
+        logging.info(f"Result = {len(result_set)}/{len(sites)}:\n- {files}")
 
 
 if __name__ == "__main__":
     set_logging()
     parser = argparse.ArgumentParser()
-    parser.add_argument("--savedir", type=str, help="save dir", default="../data")
+    parser.add_argument("--savedir", type=str, help="save dir", default=f"{BASEDIR}/data")
     parser.add_argument("--sites", nargs="+", type=str, help="List of movie sites", required=True)
     args = parser.parse_args()
     logging.info(f"args = {args}\n")
 
-    stats = download_stats(args.savedir, args.sites)
-    # if len(stats) > 0:
-    #     update_readme(stats)
+    download_stats(args.savedir, args.sites)
     logging.info(f"done\n")
