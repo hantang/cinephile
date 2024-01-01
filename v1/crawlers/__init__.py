a4a5f420b85dd0a67cdd0623a5d1e7c264e640a0
hantang <34468720+hantang@users.noreply.github.com>
2024-01-01 18:35:15 +0800

    :boom: Remove v1

--- v1/crawlers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .douban import DoubanCrawler
-from .imdb import ImdbCrawler
-from .maoyan import MaoyanCrawler
-from .mtime import MtimeCrawler
-from .tmdb import TmdbCrawler
-from .douban2 import DoubanWeeklyCrawler
